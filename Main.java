ecd10128f27fada2947d1e443732e3749daaa29c
Leo Poulson <leopoulson@gmail.com>
2020-03-27 09:30:46 +0000

    add run.sh script

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void Main() {
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Network.main(args);
+        }
+        catch (IOException e) {
+            System.out.println("io exception");
+        }
+        catch (InterruptedException e) {
+            System.out.println("interrupted exception");
+        }
     }
 }
